d6726d53bb0b56cbd065e9906d40522ab667e3f0
ryousuke.mizokami <ming10r1170@gmail.com>
2025-09-21 13:08:43 +0900

    first commit

---
+++ react-sample/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+// import App from './components/Hello';
+// import Counter from './components/Counter';
+// import { Parent } from './components/Parent';
+// import { UseMemoSample } from './components/UseMemoSample';
+// import Clock  from './components/Clock';
+// import Parent from './components/UseImperactiveHandle';
+import Input from './components/UseInput';
+// import ImageUploader from './components/UseRefSample';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+<Input />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
