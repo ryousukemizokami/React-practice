49beb4cf9fa9f8174415768fe695f69d98f5631b
ryousuke.mizokami <ming10r1170@gmail.com>
2025-09-21 11:03:28 +0900

    UseMemoSample

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,15 @@ import './index.css';
 // import App from './App';
 // import App from './components/Hello';
 // import Counter from './components/Counter';
-import { Parent } from './components/Parent';
+// import { Parent } from './components/Parent';
+import { UseMemoSample } from './components/UseMemoSample';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-<Parent />
+<UseMemoSample />
 );
 
 // If you want to start measuring performance in your app, pass a function
